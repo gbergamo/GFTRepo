766ea17adf3ece546ff81964c254940f4c1bb6eb
gbergamo <bergamo.gustavo@gmail.com>
2015-04-22 23:13:05 -0300

    changes 2

--- GFTTest/Program.cs
+++ GFTTest/Program.cs
@@ -14,6 +14,7 @@ namespace GFTTest
             { 
                 try
                 {
+                    Console.Write("Enter your order: ");
                     string rd = Console.ReadLine();
 
                     if (!string.IsNullOrEmpty(rd))
@@ -25,14 +26,33 @@ namespace GFTTest
                         }
 
                         Order o = new Order(rd);
-                        Console.WriteLine(o.Process());
+
+                        Console.WriteLine();
+                        
+                        string output = o.Process();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Output: ");
+                        Console.ResetColor();
+                        Console.WriteLine(output);
                     }
                 }
                 catch (Exception ex)
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("Valid input to time of day: morning, night");
                     Console.WriteLine("Valid input to dish types: 1 (entrée), 2 (side), 3 (drink), 4 (desserts)");
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+                finally
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
             }
         }
